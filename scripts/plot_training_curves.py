21b351fec119ffc7ad338395642bf282337658b7
Sarah L <sarahl@bu.edu>
2026-04-17 19:39:25 -0400

    added auc curves and plot for auc

--- scripts/plot_training_curves.py
+++ scripts/plot_training_curves.py
@@ -45,3 +45,34 @@ plt.savefig(save_path, dpi=300)
 plt.close()
 
 print(f"Saved plot to: {save_path}")
+
+
+
+#Auc curve 
+
+# mean auc val
+resnet=[0.8899,0.9060,0.9059,0.9229,0.9047,0.9009,0.8693,0.9215,0.8889,0.8773]
+
+densenet= [0.8423,0.8757,0.9151,0.8881,0.9303,0.8995,0.9147,0.9141,0.9296,0.9005]
+#plot
+plt.figure(figsize=(8,5))
+plt.plot(epochs, resnet, marker="o",label="ResNet-50")
+plt.plot(epochs, densenet,marker="o",label="DenseNet-121")
+
+#mark with star
+plt.plot(resnet.index(max(resnet))+1,max(resnet),"*")
+plt.plot(densenet.index(max(densenet))+1,max(densenet),"*")
+
+#label and title 
+plt.xlabel("Epoch")
+plt.ylabel("Mean Auc")
+plt.title("Validation of Mean Auc" )
+plt.legend()
+
+#save
+
+save_path =os.path.join(OUTPUT_DIR, "auc_curves.png")
+plt.savefig(save_path, dpi=300)
+plt.close()
+
+print(f"Saved plot to: {save_path}")
